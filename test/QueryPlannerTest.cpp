f8667ac95b45e7a411b39904168d3eb9b23998cf
Hannah Bast <bast@informatik.uni-freiburg.de>
2021-05-19 07:12:00 +0200

    Next try

--- test/QueryPlannerTest.cpp
+++ test/QueryPlannerTest.cpp
@@ -831,7 +831,7 @@ TEST(QueryPlannerTest, threeVarTriplesTCJ) {
     ASSERT_EQ(
         "{\n  MULTI_COLUMN_JOIN\n  {\n    SCAN FOR FULL INDEX PSO (DUMMY "
         "OPERATION)\n    qet-width: 3 \n  } join-columns: [0 & 1]\n  |X|\n  "
-        "  {\n  SCAN SPO with S = \"<s>\"\n    qet-width: 2 \n  } "
+        "{\n    SCAN SPO with S = \"<s>\"\n    qet-width: 2 \n  } "
         "join-columns: [0 & 1]\n  qet-width: 3 \n}",
         qet.asString());
   } catch (const ad_semsearch::Exception& e) {
@@ -1103,12 +1103,13 @@ TEST(QueryExecutionTreeTest, testCyclicQuery) {
     QueryPlanner qp(nullptr);
     QueryExecutionTree qet = qp.createExecutionTree(pq);
     ASSERT_EQ(
-        "{\n  MULTI_COLUMN_JOIN\n  {\n    SCAN POS with P = "
-        "\"<Film_performance>\"\n    qet-width: 2 \n  } join-columns: [0 & "
-        "1]\n  |X|\n  {\n    SORT / ORDER BY on columns:asc(1) asc(2) \n    "
+        "{\n  MULTI_COLUMN_JOIN\n  {\n    SCAN PSO with P = "
+        "\"<Spouse_(or_domestic_partner)>\"\n    qet-width: 2 \n  } "
+        "join-columns: [0 & 1]\n  |X|\n  {\n    "
+        "SORT / ORDER BY on columns:asc(1) asc(2) \n    "
         "{\n      JOIN\n      {\n        SCAN POS with P = "
-        "\"<Spouse_(or_domestic_partner)>\"\n        qet-width: 2 \n      } "
-        "join-column: [0]\n      |X|\n      {\n        SCAN PSO with P = "
+        "\"<Film_performance>\"\n        qet-width: 2 \n      } "
+        "join-column: [0]\n      |X|\n      {\n        SCAN POS with P = "
         "\"<Film_performance>\"\n        qet-width: 2 \n      } "
         "join-column: [0]\n      qet-width: 3 \n    }\n    qet-width: 3 \n  "
         "} join-columns: [1 & 2]\n  qet-width: 3 \n}",
