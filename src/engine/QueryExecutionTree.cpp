a27ed9c1e755e2daf712f803d4691970ae67ac18
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2021-06-25 09:59:31 +0200

    Allow dumping a binary relation.

--- src/engine/QueryExecutionTree.cpp
+++ src/engine/QueryExecutionTree.cpp
@@ -270,6 +270,18 @@ void QueryExecutionTree::writeTable(
         validIndices,
     std::ostream& out) const {
   shared_ptr<const ResultTable> res = getResult();
+
+  // special case : binary export of IdTable
+  if (sep == 'b') {
+    for (size_t i = from; i < upperBound; ++i) {
+      for (size_t j = 0; j < validIndices.size(); ++j) {
+        if (validIndices[j]) {
+          const auto& val = *validIndices[j];
+        out.write(reinterpret_cast<char*>(&data(i, val.first)), sizeof(Id));
+      }
+      }
+      return;
+  }
   for (size_t i = from; i < upperBound; ++i) {
     for (size_t j = 0; j < validIndices.size(); ++j) {
       if (validIndices[j]) {
