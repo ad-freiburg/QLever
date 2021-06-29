048d7564826c7b1b729c77bcc4b5bfad53285a00
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2021-06-29 10:54:30 +0200

    Removed functions that are unused when using the serialization module

--- src/index/MetaDataTypes.h
+++ src/index/MetaDataTypes.h
@@ -67,20 +67,13 @@ class FullRelationMetaData {
 
   size_t getNofElements() const;
 
-  // Restores meta data from raw memory.
-  // Needed when registering an index on startup.
-  FullRelationMetaData& createFromByteBuffer(unsigned char* buffer);
-
   // The size this object will require when serialized to file.
-  size_t bytesRequired() const;
+  //size_t bytesRequired() const;
 
   off_t getStartOfLhs() const;
 
   Id _relId;
   off_t _startFullIndex;
-
-  friend ad_utility::File& operator<<(ad_utility::File& f,
-                                      const FullRelationMetaData& rmd);
 
   // operators needed for checking of emptyness
   // inequality is the common case, so we implement this
@@ -103,14 +96,6 @@ class FullRelationMetaData {
   uint64_t _typeMultAndNofElements;
 };
 
-inline ad_utility::File& operator<<(ad_utility::File& f,
-                                    const FullRelationMetaData& rmd) {
-  f.write(&rmd._relId, sizeof(rmd._relId));
-  f.write(&rmd._startFullIndex, sizeof(rmd._startFullIndex));
-  f.write(&rmd._typeMultAndNofElements, sizeof(rmd._typeMultAndNofElements));
-  return f;
-}
-
 class BlockBasedRelationMetaData {
  public:
   BlockBasedRelationMetaData();
@@ -119,11 +104,7 @@ class BlockBasedRelationMetaData {
                              const vector<BlockMetaData>& blocks);
 
   // The size this object will require when serialized to file.
-  size_t bytesRequired() const;
-
-  // Restores meta data from raw memory.
-  // Needed when registering an index on startup.
-  BlockBasedRelationMetaData& createFromByteBuffer(unsigned char* buffer);
+  //size_t bytesRequired() const;
 
   // Takes a LHS and returns the offset into the file at which the
   // corresponding block can be read as well as the nof bytes to read.
@@ -152,16 +133,6 @@ void serialize(Serializer& serializer, BlockBasedRelationMetaData& metaData) {
   serializer& metaData._startRhs;
   serializer& metaData._offsetAfter;
   serializer& metaData._blocks;
-}
-
-inline ad_utility::File& operator<<(ad_utility::File& f,
-                                    const BlockBasedRelationMetaData& rmd) {
-  f.write(&rmd._startRhs, sizeof(rmd._startRhs));
-  f.write(&rmd._offsetAfter, sizeof(rmd._offsetAfter));
-  auto nofBlocks = rmd._blocks.size();
-  f.write(&nofBlocks, sizeof(nofBlocks));
-  f.write(rmd._blocks.data(), nofBlocks * sizeof(BlockMetaData));
-  return f;
 }
 
 class RelationMetaData {
@@ -193,9 +164,3 @@ class RelationMetaData {
   const FullRelationMetaData& _rmdPairs;
   const BlockBasedRelationMetaData* _rmdBlocks;
 };
-
-inline ad_utility::File& operator<<(ad_utility::File& f,
-                                    const RelationMetaData& rmd) {
-  f << rmd._rmdPairs << *rmd._rmdBlocks;
-  return f;
-}
